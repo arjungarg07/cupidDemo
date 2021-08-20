b3af03c3f756bf42792701faec1ca2345ae711b0
arjungarg07 <arjun.garg.cse01@gmail.com>
2021-08-20 17:34:58 +0530

    fix: path error and input array

--- index.js
+++ index.js
@@ -2,27 +2,17 @@ const appRelationsDiscovery = require('@asyncapi/app-relations-discovery');
 
 const {getAsyncApiExamples} = require('./util'); 
 
-let flightServicedocs = getAsyncApiExamples();
+let flightServiceDocs = getAsyncApiExamples();
 
 // For default output syntax
 // const defaultOutput = appRelationsDiscovery.getRelations(...docs);
 // console.log(defaultOutput);
 
-// For mermaid Flowchart 
-console.log(flightServicedocs);
 (async()=>{
     try{
-        const mermaidFlowchart = await appRelationsDiscovery.getRelations(...flightServicedocs,{syntax:'mermaid'});
-        console.log(mermaidFlowchart);
+        const output = await appRelationsDiscovery.getRelations(flightServiceDocs,{syntax:'reactFlow'});
+        console.log(output);
     } catch(err){
         console.log(err);
     }
 })();
-
-// For plantUML classDiagram 
-// const plantUMLClassDiagram = appRelationsDiscovery.getRelations(...docs,{syntax:'plantUML'});
-// console.log(plantUMLClassDiagram);
-
-// For reactFlow nodes
-// const reactFlowNodes = appRelationsDiscovery.getRelations(...docs,{syntax:'reactFlow'});
-// console.log(reactFlowNodes);
